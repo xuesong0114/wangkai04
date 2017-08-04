0537fe13f6b22836da419fbfc16bb835dee91feb
perfectiss <wangkai9453@126.com>
2017-08-04 19:18:17 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcoo/tcmp056/test/Test.java
@@ -0,0 +1,12 @@
+package cn.bdqn.tangcoo.tcmp056.test;
+
+/**
+ * Created by wangkair on 2017/8/4.
+ */
+public class Test {
+
+    @org.junit.Test
+    public void testMain() {
+        System.out.println("hello world");
+    }
+}
